cb4142d4ab852247bae899dee7d5ec2dc42ca56e
valero <evalerovilella@gmail.com>
2023-11-01 21:53:51 +0100

    pages to CBV

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.utils import timezone
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Page
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    model = Page
+    template_name = "pages/page_list.html"
+    paginate_by = 5
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["now"] = timezone.now()
+    #     return context
+    
+    
+# def pages(request):
+#     pages = get_list_or_404(Page)
+#     return render(request, 'pages/pages.html', {'pages':pages})
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageDetailView(DetailView):
+    model = Page
+
+# def page(request, page_id, page_slug):
+#     page = get_object_or_404(Page, id=page_id)
+#     return render(request, 'pages/page.html', {'page':page})
