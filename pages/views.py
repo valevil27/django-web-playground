03c47a8e330aa5b9e58e7a2ca6e6517118f98335
valero <evalerovilella@gmail.com>
2023-11-02 12:19:59 +0100

    added create page form

--- pages/views.py
+++ pages/views.py
@@ -1,28 +1,24 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Page
+
 
 # Create your views here.
 class PageListView(ListView):
     model = Page
     template_name = "pages/page_list.html"
     paginate_by = 5
-    
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["now"] = timezone.now()
-    #     return context
-    
-    
-# def pages(request):
-#     pages = get_list_or_404(Page)
-#     return render(request, 'pages/pages.html', {'pages':pages})
+
 
 class PageDetailView(DetailView):
     model = Page
 
-# def page(request, page_id, page_slug):
-#     page = get_object_or_404(Page, id=page_id)
-#     return render(request, 'pages/page.html', {'page':page})
+
+class PageCreate(CreateView):
+    model = Page
+    fields = ["title", "content", "order"]
+    success_url = reverse_lazy('pages:pages')
