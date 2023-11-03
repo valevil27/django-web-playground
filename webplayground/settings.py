2f8a0f9c058da5ecbe0ff856d563e60890fd5d1d
valero <evalerovilella@gmail.com>
2023-11-03 19:34:01 +0100

    debug mail local smtp server password reset templates

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +129,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Email
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_mails")
+else:
+    # Here goes deployment setup
+    pass
