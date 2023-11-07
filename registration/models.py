bf042ee2e7d17f4e91b5e73ace0002439f280a01
valero <evalerovilella@gmail.com>
2023-11-07 11:58:56 +0100

    profile avatar upload functionallity

--- registration/models.py
+++ registration/models.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+def avatar_upload_to(instance: models.Model, filename: str):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return "profile_images/" + filename
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE) # type: ignore
-    avatar = models.ImageField("Avatar", upload_to="profile_images", null=True, blank=True)
+    avatar = models.ImageField("Avatar", upload_to=avatar_upload_to, null=True, blank=True)
     bio = models.TextField("Biografía", null=True,blank=True)
     link = models.URLField("Portfolio", max_length=254, null=True, blank=True)
     
