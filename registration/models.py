7311fdf2beca9b98eff0ef6abdf7270060e5eb41
valero <evalerovilella@gmail.com>
2023-11-07 11:35:32 +0100

    Signal test

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,3 +9,10 @@ class Profile(models.Model):
     avatar = models.ImageField("Avatar", upload_to="profile_images", null=True, blank=True)
     bio = models.TextField("Biografía", null=True,blank=True)
     link = models.URLField("Portfolio", max_length=254, null=True, blank=True)
+    
+@receiver(post_save, sender=User)
+def profile_on_user_creation(sender, instance, **kwargs):
+    if kwargs.get("created", False):
+        Profile.objects.get_or_create(user=instance)
+        print("Se ha creado un usuario y un perfil enlazado.")
+    
--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    #? Prepara las pruebas
+    def setUp(self):
+        User.objects.create_user("test","test@test.com","test1234")
+    
+    #? Realiza la prueba indicada
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='test').exists()
+        self.assertEqual(exists,True)
