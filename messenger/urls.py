73159d7d9013f2a15e86827d253548c9327ff428
valero <evalerovilella@gmail.com>
2023-11-09 19:44:17 +0100

    added view config for messenger

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from messenger.views import ThreadDetailView, ThreadView
+
 
 messenger_patterns = ([
     
+    path("", ThreadView.as_view(), name="list"),
+    path("thread/<int:pk>", ThreadDetailView.as_view(), name="detail"),
     ],"messenger")
--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from __future__ import annotations
+from django.http import Http404
+from django.views.generic import ListView, DetailView, TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+from messenger.models import Thread
 
 # Create your views here.
+
+@method_decorator(login_required, "dispatch")
+class ThreadView(TemplateView):
+    template_name = "messenger/thread_list.html"
+
+@method_decorator(login_required, "dispatch")
+class ThreadDetailView(DetailView):
+    model = Thread
+    template_name = "messenger/thread_detail.html"
+    
+    def get_object(self, queryset = None):
+        obj: Thread = super().get_object(queryset) # type: ignore
+        if self.request.user not in obj.users.all():
+            return Http404()
+        return obj
+            
