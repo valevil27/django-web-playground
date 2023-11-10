6f54ac2f80e36068d745078dc44b41f4ce2d9068
valero <evalerovilella@gmail.com>
2023-11-10 13:51:20 +0100

    Asynchronous send message added

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from messenger.views import ThreadDetailView, ThreadView
+from messenger.views import ThreadDetailView, ThreadView, add_message
 
 
 messenger_patterns = ([
-    
     path("", ThreadView.as_view(), name="list"),
-    path("thread/<int:pk>", ThreadDetailView.as_view(), name="detail"),
+    path("thread/<int:pk>/", ThreadDetailView.as_view(), name="detail"),
+    path("thread/<int:pk>/add/", add_message, name="add"),
     ],"messenger")
--- messenger/views.py
+++ messenger/views.py
@@ -1,26 +1,40 @@
 from __future__ import annotations
-from django.http import Http404
+from django.http import Http404, HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-
-
-from messenger.models import Thread
+from messenger.models import Thread, Message
 
 # Create your views here.
+
 
 @method_decorator(login_required, "dispatch")
 class ThreadView(TemplateView):
     template_name = "messenger/thread_list.html"
 
+
 @method_decorator(login_required, "dispatch")
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = "messenger/thread_detail.html"
-    
-    def get_object(self, queryset = None):
-        obj: Thread = super().get_object(queryset) # type: ignore
+
+    def get_object(self, queryset=None):
+        obj: Thread = super().get_object(queryset)  # type: ignore
         if self.request.user not in obj.users.all():
             return Http404()
         return obj
-            
+
+
+def add_message(request: HttpRequest, pk: int):
+    json_resp = {"created": False}
+    if not request.user.is_authenticated:
+        raise Http404("User is not authenticated")
+    content = request.GET.get("content", None)
+    if not content:
+        return JsonResponse(json_resp)
+    thread = get_object_or_404(Thread, pk=pk)
+    message = Message.objects.create(user=request.user, content=content)
+    thread.messages.add(message)
+    json_resp["created"] = True
+    return JsonResponse(json_resp)
